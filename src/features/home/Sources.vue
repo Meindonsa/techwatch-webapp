8c7f21e2a236d5ed1756756e70312f1dffea5e63
borisaxel <boris.ebaneth@ossas-it.com>
2025-12-29 23:08:34 +0100

    feat: Display sources list

--- src/features/home/Sources.vue
+++ src/features/home/Sources.vue
@@ -1,9 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at aut cupiditate dolor, dolore
-  ducimus expedita iusto laudantium minima minus odio perspiciatis quaerat, rem tempore tenetur
-  vitae voluptas. Quos, reprehenderit!
+  <div
+    class="w-full max-w-sm p-6 bg-neutral-primary-soft border border-default rounded-base shadow-xs"
+  >
+    <h5 class="mb-4 text-xl font-semibold leading-none text-heading text-white">Sources</h5>
+    <div class="flow-root">
+      <ul role="list" class="divide-default text-gray-300">
+        <li
+          class="text-sm text-body truncate mb-2 hover:text-indigo-500 transition ease-in-out duration-300"
+        >
+          <a class="cursor-pointer hover:underline hover:underline-offset-4">email@windster.com</a>
+        </li>
+        <li
+          class="text-sm text-body truncate mb-2 hover:text-indigo-500 transition ease-in-out duration-300"
+        >
+          <a class="cursor-pointer hover:underline hover:underline-offset-4">email@windster.com</a>
+        </li>
+        <li
+          class="text-sm text-body truncate mb-2 hover:text-indigo-500 transition ease-in-out duration-300"
+        >
+          <a class="cursor-pointer hover:underline hover:underline-offset-4">email@windster.com</a>
+        </li>
+        <li
+          class="text-sm text-body truncate mb-2 hover:text-indigo-500 transition ease-in-out duration-300"
+        >
+          <a class="cursor-pointer hover:underline hover:underline-offset-4">email@windster.com</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- src/shared/components/Navbar.vue
+++ src/shared/components/Navbar.vue
@@ -23,12 +23,12 @@ const onFilter = (name: string) => {
 <template>
   <nav class="bg-gray-950 fixed w-full z-20 top-0 start-0 border-b border-b-gray-900">
     <div class="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <RouterLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-7" alt="Flowbite Logo" />
         <span class="self-center text-xl text-white text-heading font-semibold whitespace-nowrap">
           Flowbite
         </span>
-      </a>
+      </RouterLink>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary"
